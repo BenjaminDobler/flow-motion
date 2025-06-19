11037cd6285e9822f719331d8114a54be8655638
Benjamin Dobler <benjamin.dobler@gmail.com>
2025-06-19 21:10:44 +0200

    fix: fixed routing on gh-pages

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 WIP: Connecting containers to visualize relationships between objects (e.g. class diagrams...)
 
-[Demo](https://benjamindobler.github.io/ng-bond/simple)
+[Demo](https://benjamindobler.github.io/ng-bond)
--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -2,10 +2,10 @@ import {
   ApplicationConfig,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection(), provideRouter(routes)],
+  providers: [provideZonelessChangeDetection(), provideRouter(routes, withHashLocation())],
 };
